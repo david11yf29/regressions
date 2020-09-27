42c0b23aced452353c2cf08308776c31fc7d4e26
david11yf29 <david11yf29@gmail.com>
2020-09-27 22:40:04 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const tf = require('@tensorflow/tfjs');
+const loadCSV = require('./load-csv');
+
+const { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
+  shuffle: true,
+  splitTest: 50,
+  dataColumns: ["horsepower"],
+  labelColumns: ["mpg"]
+});
+
+// console.log(features, labels);
