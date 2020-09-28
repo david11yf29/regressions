dd857084a627686c373ba17d350b21f8e44f8b99
david11yf29 <david11yf29@gmail.com>
2020-09-28 14:48:46 +0800

    updated

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const tf = require('@tensorflow/tfjs');
 const loadCSV = require('./load-csv');
+const LinearRegression = require('./linear-regression');
 
 const { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
   shuffle: true,
@@ -9,3 +10,11 @@ const { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
 });
 
 // console.log(features, labels);
+
+const regression = new LinearRegression(features, labels, {
+  learningRate: 0.001, iterations: 1
+});
+
+regression.train();
+
+console.log('Updated M is: ', regression.m, 'Updated B is: ', regression.b);
