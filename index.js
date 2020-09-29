532ef16b928d3215565501414d1a327e79f913f1
david11yf29 <david11yf29@gmail.com>
2020-09-29 17:59:07 +0800

    updated

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
 // console.log(features, labels);
 
 const regression = new LinearRegression(features, labels, {
-  learningRate: 0.001, iterations: 1
+  learningRate: 0.0001, iterations: 100
 });
 
 regression.train();
