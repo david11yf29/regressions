1f200c2a5d63d4feba37d6a5d5b1cd2adfa3f86e
david11yf29 <david11yf29@gmail.com>
2020-09-30 16:46:30 +0800

    updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const tf = require('@tensorflow/tfjs');
 const loadCSV = require('./load-csv');
 const LinearRegression = require('./linear-regression');
+const plot = require('node-remote-plot');
 
 const { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
   shuffle: true,
@@ -18,7 +19,14 @@ const regression = new LinearRegression(features, labels, {
 regression.train();
 const r2 = regression.test(testFeatures, testLabels);
 
-console.log('R2 is', r2);
+plot({
+  x: regression.mseHistory.reverse(),
+  xLabel: 'Iteration #',
+  yLabel: 'Mean Squared Error'
+})
+
+// console.log("MSE History", regression.mseHistory);
+// console.log('R2 is', r2);
 
 // console.log('Updated M is: ', regression.weights.arraySync()[1], 'Updated B is: ', regression.weights.arraySync()[0]);
 
